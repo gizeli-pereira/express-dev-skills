47f8619f191c5f9c75fd6ca4a9f6f64a6402fcda
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-03-30 10:36:47 -0500

    Add partials

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,11 +2,20 @@
  const Skill = require('../models/skill');
 
 module.exports = {
-    index
+    index,
+    show
   };
+
+function show(req, res) {
+    res.render('skills/show', {
+        skill: Skill.getOne(req.params.id),
+        title: 'Skills Details'
+    });
+}
 
 function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title: 'All Skills'
     });
   }
